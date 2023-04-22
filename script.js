7ca799668ab7175ca7408744b7b6d72df5baf9aa
MoisesAvilar <124001963+MoisesAvilar@users.noreply.github.com>
2023-04-21 21:17:21 -0300

    .

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-function clickMenu() {
-    if (itens.style.display == 'block') {
-        itens.style.display = "none"
-    } else {
-        itens.style.display = 'block'
-    }
-}
+// seleciona o link do botão "Comprar"
+const botaoComprar = document.querySelector('#buy');
 
-function mudoutamanho(){
-    if (window.innerWidth >= 768) {
-        itens.style.display = 'block'
-    } else {
-        itens.style.display = 'none'
-    }
-}
+// adiciona um evento de clique ao botão "Comprar"
+botaoComprar.addEventListener('click', (event) => {
+  // evita que o link padrão seja seguido
+  event.preventDefault();
+  
+  // obtém o nome e o preço da roupa da tag div
+  const info = document.querySelector('#info');
+  const nomeRoupa = info.querySelector('h2').textContent;
+  const precoRoupa = info.querySelector('p').textContent;
+
+  // constrói a mensagem pré-preenchida
+  const mensagem = `Olá, gostaria de comprar a ${nomeRoupa} por ${precoRoupa}.`;
+
+  // abre o WhatsApp com a mensagem pré-preenchida
+  window.location.href = `whatsapp://send?text=${encodeURIComponent(mensagem)}`;
+});
